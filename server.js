35bfbdb6bab690597925bb35063328f1fa05740a
Milan Sony <milansonyofficial@gmail.com>
2025-04-19 21:22:35 +0530

    feat: mongoDB connection added

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ import express from "express"
 import dotenv from "dotenv"
 import routes from "./routes/index.js"
 import bodyParser from "body-parser"
+import connectDB from "./configs/db.js"
 
 // config .env
 dotenv.config()
 
 // creates an express app
 const app = express()
+
+// connect with DB
+connectDB()
 
 // body parser
 // parse application/x-www-form-urlencoded
